5985d502da33bff2f9b7b60bee805c8b0395102b
open94006 <open94006880103@gmail.com>
2022-01-14 23:47:56 +0800

    更新 自我介紹&自傳

--- src/Intro.js
+++ src/Intro.js
@@ -1,5 +1,5 @@
 import user from "./data/Daniel_2022.png";
-import { ImMail, ImFacebook2, ImGithub } from 'react-icons/im';
+import { ImMail, ImFacebook2, ImGithub, ImBook, ImBriefcase } from 'react-icons/im';
 
 const Intro = () => {
     return (
@@ -9,13 +9,8 @@ const Intro = () => {
             </div>
             <div>
                 <h2>林致嘉 Daniel Lin</h2>
-                <p>畢業於<b>高雄科技大學資訊管理系</b>，行事風格習慣規劃後並踏實完成。</p>
-                <p>大學期間曾任系學會副會長、系籃隊長、班級代表、畢業代表等職務，曾任高雄雙欣科技<b>實習PHP工程師半年</b>。
-                </p>
-                <hr />
-            </div>
-            <div>
-                <h4>聯絡方式</h4>
+                <p><ImBook />&ensp;國立高雄科技大學—資訊管理系</p>
+                <p><ImBriefcase />&ensp;高雄雙欣科技產學合作—後端工程師</p>
                 <p><ImMail />&ensp;open94006880103@gmail.com</p>
                 <p><ImFacebook2 />&ensp;
                     <a href="https://www.facebook.com/open94006/"
